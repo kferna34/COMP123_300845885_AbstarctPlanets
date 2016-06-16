20ad4b893e764c357a07421c78d81b558f79d88b
kferna34 <kferna34@my.centennialcollege.ca>
2016-06-16 13:24:41 -0400

    Created abstract planet class. -- initial push

---
+++ COMP123_300845885_AbstarctPlanets/Planet.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_300845885_AbstarctPlanets
+{
+    public abstract class Planet
+    {
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        public double Diameter
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+        }
+
+        public double Mass
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public double RotationalPeriod
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+        public   Planet(string name,double diameter,double mass)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        
+
+        
+
+    }
+}
