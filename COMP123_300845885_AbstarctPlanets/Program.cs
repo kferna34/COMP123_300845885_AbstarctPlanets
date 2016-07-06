2f2f3bdb0f936f8ccc31a695ae87bb120bade890
kferna34 <kferna34@my.centennialcollege.ca>
2016-07-06 16:23:25 -0400

    final== just more internal documentation needed

--- COMP123_300845885_AbstarctPlanets/Program.cs
+++ COMP123_300845885_AbstarctPlanets/Program.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+    * Author: Krytia Fernandez, 300845885
+    * Date Last modified : july 6,2016
+    * Description: a program that implements the classes and interfaces
+    * version : 0.0.0.10 --final== just more internal documentation needed
+    * 
+    */
 namespace COMP123_300845885_AbstarctPlanets
 {/// <summary>
 /// this is the driver class
@@ -12,16 +18,23 @@ namespace COMP123_300845885_AbstarctPlanets
     {
         static void Main(string[] args)
         {
-            
-
-
-
+            Console.WriteLine("===================================");
+            GiantPlanet gPlanet = new GiantPlanet("CodingPlanet",52222000, 0000000, "dark");
+            Console.WriteLine(gPlanet.ToString());
+            Console.WriteLine("===================================");
+            Console.WriteLine();
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            TerrestrialPlanet tPlanet = new TerrestrialPlanet("pluto", 2365, 8954, true);
+            Console.WriteLine(tPlanet.ToString());
+            Console.WriteLine();
+            waitForAnyKey();
 
         }
 
-        public void waitForAnyKey()
+        public static void waitForAnyKey()
         {
-            throw new System.NotImplementedException();
+            Console.WriteLine("PRESS ANY KEY TO CONTINUE");
+            Console.ReadKey();
         }
     }
 }
--- COMP123_300845885_AbstarctPlanets/TerrestrialPlanet.cs
+++ COMP123_300845885_AbstarctPlanets/TerrestrialPlanet.cs
@@ -5,26 +5,58 @@ using System.Text;
 
 namespace COMP123_300845885_AbstarctPlanets
 {
+    /// <summary>
+    /// this class is a subclass of the planet abstract class. will also implement ihasmoon and ihabitable interfaces
+    /// </summary>
     public class TerrestrialPlanet : Planet, IHasMoons, IHabitable
     {
-        bool _hasMoons;
-        private bool _oxygen;
-
         public TerrestrialPlanet(string name, double diameter, double mass, string type) : base(name, diameter, mass)
         {
-            
+            this.Oxygen = Oxygen;
         }
 
+            private bool _oxygen;
+
+        //~~~~~~~~~~~~~~~~~~~~~~~PUBLIC PROPERTY~~~~~~~~~~~~~~~~~~~~~~
+        /// <summary>
+        /// THIS IS A PROPERTY FOR _OXYGEN FIELD
+        /// </summary>
+        public bool Oxygen
+        {
+            get
+            {
+                return this._oxygen;
+            }
+            set
+            {
+                this._oxygen = value;
+            }
+        }
+        /// <summary>
+        /// this public method Habitable() will return true if the oxygen instance variable is set to true
+        /// </summary>
+        /// <returns></returns>
         public bool Habitable()
         {
-            throw new System.NotImplementedException();
+            if (Oxygen == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
+        /// <summary>
+        /// this public method HasMethod() will return true if the moonCount property is greater than zero
+        /// </summary>
+        /// <returns></returns>
         public bool HasMoons()
         {
             if (MoonCount != 0)
             {
-                return this._hasMoons;
+                return true;
             }
             else
             {
